c13ba5c9e1fdd6ce4b58c0dd68ecaac447336b21
yicode <120433484+yuki-845@users.noreply.github.com>
2024-09-16 04:36:13 +0900

    initila commit

---
+++ Live2DSample/KeyboardResponder.swift
@@ -0,0 +1,8 @@
+//
+//  KeyboardResponder.swift
+//  Live2DSample
+//
+//  Created by 平井悠貴 on 2024/09/15.
+//
+
+import Foundation
---
+++ Live2DSample/TodoItem.swift
@@ -0,0 +1,18 @@
+//
+//  TodoItem.swift
+//  Live2DSample
+//
+//  Created by 平井悠貴 on 2024/09/15.
+//
+
+import SwiftUI
+
+struct TodoItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TodoItem()
+}
