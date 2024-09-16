384620cf076a2337e61f35776b94c1d4c9f3d98d
yicode <120433484+yuki-845@users.noreply.github.com>
2024-09-16 12:35:49 +0900

    commit change

---
+++ Live2DSample/CharacterSelectView.swift
@@ -0,0 +1,20 @@
+//
+//  CharcterSelectView.swift
+//  Live2DSample
+//
+//  Created by 平井悠貴 on 2024/09/16.
+//
+
+import SwiftUI
+
+struct CharcterSelectView: View {
+    var body: some View {
+        Live2DView()
+            .shadow(color: Color.red, radius: 0, x: -7, y: 0)
+            
+    }
+}
+
+#Preview {
+    CharcterSelectView()
+}
---
+++ Live2DSample/TabedView.swift
@@ -0,0 +1,32 @@
+//
+//  TavView.swift
+//  Live2DSample
+//
+//  Created by 平井悠貴 on 2024/09/16.
+//
+
+import SwiftUI
+
+struct TabView: View {
+    var body: some View {
+        TabView {
+            ContentView()
+                .tabItem {
+                    Image(systemName: "1.circle")
+                    Text("First")
+                }
+            
+            CharacterSelectView()
+                .tabItem {
+                    Image(systemName: "2.circle")
+                    Text("Second")
+                }
+                .badge(5)
+            
+        }
+    }
+}
+
+#Preview {
+    TabView()
+}
