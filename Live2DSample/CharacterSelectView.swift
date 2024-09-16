fd2192cb6f1c6c2b31d3779a4836126c2ec2e517
yicode <120433484+yuki-845@users.noreply.github.com>
2024-09-17 01:22:27 +0900

    コミット

--- Live2DSample/CharacterSelectView.swift
+++ Live2DSample/CharacterSelectView.swift
@@ -11,24 +11,60 @@ struct CharacterSelectView: View {
     @State private var kyara = "Hiyori"  // キャラクター名を管理するState変数
     var body: some View {
         GeometryReader { geometry in
-            if(kyara == "Hiyori") {
-                HiyoriView()
-                    .shadow(color: Color.red, radius: 0, x: -7, y: 0)
-                    .frame(width: geometry.size.width, height: geometry.size.height)
-                    .position(x: geometry.size.width / 2, y: geometry.size.height / 2.2)
-            }else {
-                HaruView()
-                    .shadow(color: Color.red, radius: 0, x: -7, y: 0)
-                    .frame(width: geometry.size.width, height: geometry.size.height)
-                    .position(x: geometry.size.width / 2, y: geometry.size.height / 2.2)
+            ZStack {
+                ZStack {
+                                // 画像にマスクを適用
+                                Image("RUKA 1")
+                                   
+                                    
+                                    
+                                    .offset(x: geometry.size.width * 0.13, y: geometry.size.height * 1.1) // 位置をマスクと同じに調整
+                                    .mask(
+                                        Rectangle()
+                                            .frame(width: 2000, height: geometry.size.height / 4.2)
+                                            .rotationEffect(.degrees(-47)) // -47度回転
+                                            .offset(x: 50, y: -geometry.size.height * 0.25) // マスクの位置調整
+                                    )
+                                
+                                // 透明な四角形を同じ位置に配置
+                                Rectangle()
+                                    .frame(width: 2000, height: geometry.size.height / 4.2)
+                                    .rotationEffect(.degrees(-47)) // -47度回転
+                                    .offset(x: 50, y: -geometry.size.height * 0.25) // 位置をマスクと同じに調整
+                                    .foregroundColor(.red) // 四角形の色
+                                    .opacity(0.6) // 透明度
+                            }
+                            .frame(width: geometry.size.width, height: geometry.size.height) // ZStackのサイズ調整
+                
+                if(kyara == "Hiyori") {
+                    HiyoriView()
+                        .shadow(color: Color.red, radius: 0, x: -7, y: 0)
+                        .frame(width: geometry.size.width, height: geometry.size.height)
+                        .position(x: geometry.size.width / 2, y: geometry.size.height / 2.2)
+                    
+                }else {
+                    HaruView()
+                        .shadow(color: Color.red, radius: 0, x: -7, y: 0)
+                        .frame(width: geometry.size.width, height: geometry.size.height)
+                        .position(x: geometry.size.width / 2, y: geometry.size.height / 2.2)
+                    
+                }
             }
-            
-            Button {
-                kyara = "Haru"
-            } label: {
-                Text("はるよこい")
+            ZStack {
+                Image("RUKAPASS")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .offset(x: -7, y: 7)
+                Image("RUKA2")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
                 
             }
+            .frame(width: geometry.size.width / 2)
+            .position(x: geometry.size.width / 2, y: geometry.size.height / 1.5)
+            
+            
+            
         }
     }
 }
--- Live2DSample/TabedView.swift
+++ Live2DSample/TabedView.swift
@@ -8,18 +8,30 @@
 import SwiftUI
 
 struct TabeView: View {
+   
+            //TabViewの背景色の設定（青色）
+    init() {
+        let appearance: UITabBarAppearance = UITabBarAppearance()
+        appearance.backgroundColor = UIColor(red: 20 / 255, green: 54 / 255, blue: 62 / 255, alpha: 1.0)
+
+        UITabBar.appearance().scrollEdgeAppearance = appearance
+        UITabBar.appearance().standardAppearance = appearance
+    }
+        
     var body: some View {
         TabView {
             ContentView()
                 .tabItem {
                     Image(systemName: "1.circle")
-                    Text("First")
+                        .foregroundColor(.white)
+                    Text("Todo")
                 }
             
             CharacterSelectView()
                 .tabItem {
                     Image(systemName: "2.circle")
-                    Text("Second")
+                        .foregroundColor(.white)
+                    Text("キャラ選択")
                 }
                 .badge(5)
             
